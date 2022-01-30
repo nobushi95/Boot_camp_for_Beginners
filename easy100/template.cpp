c40c15137022676460c0a84668c60fb7db088db5
nobushi95 <nobushi1121@gmail.com>
2022-01-30 19:48:22 +0900

    add REPEQ macro

--- easy100/template.cpp
+++ easy100/template.cpp
@@ -5,6 +5,7 @@ using namespace std;
 using ll = long long;
 
 #define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
 #define RREP(i, right, left) for (ll i = right; i >= left; i--)
 #define FORE(elem, container) for (auto & elem : container)
 #define ALL(x) (x).begin(), (x).end()
