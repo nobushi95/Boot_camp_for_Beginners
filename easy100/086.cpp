3ff9d09764ce4add245b19edfcc41d250de0a332
nobushi95 <nobushi1121@gmail.com>
2022-02-16 23:02:19 +0900

    add easy100/086

---
+++ easy100/086.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    string S;
+    ll K;
+    cin >> S >> K;
+
+    int c = 0;
+    FORE(s, S) {
+        if (s != '1') {
+            cout << s << endl;
+            return 0;
+        }
+        c++;
+        if (c == K) {
+            cout << 1 << endl;
+            return 0;
+        }
+    }
+
+    return 0;
+}
