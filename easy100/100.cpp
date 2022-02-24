1a2953debd7399d8bd222d61d033a5f92fbd049d
nobushi95 <nobushi1121@gmail.com>
2022-02-24 20:52:49 +0900

    add easy100/100

---
+++ easy100/100.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> a(N);
+    FORE(ai, a) cin >> ai;
+
+    ll ans = 0;
+    REP(i, 0, N) {
+        if (i + 1 == a[a[i] - 1]) {
+            ans++;
+        }
+    }
+
+    cout << ans / 2 << endl;
+
+    return 0;
+}
