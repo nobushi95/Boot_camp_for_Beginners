3849f036943097bfea612bd68178ffe6253e3271
nobushi95 <nobushi1121@gmail.com>
2022-03-06 16:06:08 +0900

    add medium100/020

---
+++ medium100/020.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    FORE(a, A) cin >> a;
+
+    map<ll, bool> tbl;
+    FORE(a, A) {
+        if (tbl[a]) tbl[a] = false;
+        else tbl[a] = true;
+    }
+
+    ll count = 0;
+    FORE(t, tbl) {
+        if (t.second) count++;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
