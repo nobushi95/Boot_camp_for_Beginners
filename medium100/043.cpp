a7d9aafe59371222a8ea4b07ebb09cd951ac4fe8
nobushi95 <nobushi1121@gmail.com>
2022-03-19 16:57:35 +0900

    add medium100/043

---
+++ medium100/043.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll X;
+    cin >> X;
+
+    ll ans = 0;
+    ll x = 0;
+    while (x < X) {
+        ans++;
+        x += ans;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
