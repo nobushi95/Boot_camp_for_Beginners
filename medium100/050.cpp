e182e51f17a125d7dcab3654ead664f06e700c4f
nobushi95 <nobushi1121@gmail.com>
2022-03-22 23:04:46 +0900

    add medium100/050

---
+++ medium100/050.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll L, R;
+    cin >> L >> R;
+    
+    if (R - L > 2018) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    ll ans = LLONG_MAX;
+    REP(l, L, R) {
+        REPEQ(r, l + 1, R) {
+            ll cur = (l * r) % 2019;
+            chmin(ans, cur);
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
