5eb5a01eb22a251df0b207fcd3c05a154b233235
nobushi95 <nobushi1121@gmail.com>
2022-03-29 23:11:10 +0900

    add medium100/064

---
+++ medium100/064.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> a(N);
+    FORE(ai, a) cin >> ai;
+
+    map<ll, ll> tbl;
+    FORE(ai, a) tbl[ai]++;
+
+    ll ans = 0;
+    FORE(t, tbl) {
+        if (t.first == t.second) continue;;
+        if (t.first < t.second) ans += t.second - t.first;
+        else ans += t.second;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
