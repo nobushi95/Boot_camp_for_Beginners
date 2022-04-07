8caf1672eaf4ced07fec558f87d238e948b8e992
nobushi95 <nobushi1121@gmail.com>
2022-04-07 19:32:40 +0900

    add medium100/080

---
+++ medium100/080.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N;
+    cin >> N;
+    string S;
+    cin >> S;
+
+    vector<ll> alpha(26, 0);
+    FORE(s, S) alpha[s - 'a']++;
+    ll ans = 1;
+    REP(i, 0, 26) {
+        ans *= alpha[i] + 1;
+        ans %= 1000000007;
+    }
+    ans--;
+    cout << ans << endl;
+
+    return 0;
+}
