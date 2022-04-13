4b6d93b44b17c7cc5e66946aba744d622415fb60
nobushi95 <nobushi1121@gmail.com>
2022-04-13 22:48:58 +0900

    add medium100/094

---
+++ medium100/094.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll A, B, X;
+    cin >> A >> B >> X;
+
+    ll left = 0, right = 1000000001;
+    while (left + 1 != right) {
+        ll mid = (left + right) / 2;
+        ll d = to_string(mid).length();
+        ll x = A * mid + B * d;
+        if (x <= X) left = mid;
+        else right = mid;
+    }
+
+    cout << left << endl;
+
+    return 0;
+}
