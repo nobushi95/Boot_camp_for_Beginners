152986aca7d152c94d28b86baee217658c49b98e
nobushi95 <nobushi1121@gmail.com>
2022-05-18 17:46:08 +0900

    add hard100/030

---
+++ hard100/030.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> A(N);
+    FORE(a, A) cin >> a;
+
+    sort(ALL(A));
+    ll M = A.back();
+
+    ll G = A.front();
+    REP(i, 1, N) {
+        G = gcd(G, A[i]);
+    }
+
+    if (K <= M && K % G == 0) cout << "POSSIBLE" << endl;
+    else cout << "IMPOSSIBLE" << endl;
+
+    return 0;
+}
