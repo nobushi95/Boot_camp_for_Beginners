46da6c0c35d3116aa61c7a98809c97bfda67b161
nobushi95 <nobushi1121@gmail.com>
2022-06-13 21:18:56 +0900

    mod template.cpp

--- hard100/template.cpp
+++ hard100/template.cpp
@@ -7,6 +7,7 @@ using ll = long long;
 #define RREP(i, right, left) for (ll i = right; i >= left; i--)
 #define FORE(elem, container) for (auto & elem : container)
 #define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
 template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
 template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
 template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
