6ead16a14845b680993ce38f0a1bcb9f6fcaef48
nobushi95 <nobushi1121@gmail.com>
2022-06-19 11:30:21 +0900

    add hard100/046

---
+++ hard100/046.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    string X;
+    cin >> X;
+
+    string ans;
+    ans.reserve(200000);
+
+    FORE(x, X) {
+        if (x == 'S') ans.push_back(x);
+        else {
+            if (ans.back() == 'S') ans.pop_back();
+            else ans.push_back(x);
+        }
+    }
+
+    cout << ans.length() << endl;
+
+    return 0;
+}
