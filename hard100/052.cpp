2364fd860cf516c5a33410886f457c92a97e3285
nobushi95 <nobushi1121@gmail.com>
2022-07-04 22:18:52 +0900

    add hard100/052

---
+++ hard100/052.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+vector<ll> makeEnumDiv(ll n) {
+    vector<ll> ret;
+    for (ll i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (n / i != i) ret.push_back(n / i);
+        }
+    }
+    sort(ALL(ret));
+    return ret;
+}
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+
+    auto enumDiv = makeEnumDiv(M);
+    ll ans = 1;
+    FORE(e, enumDiv) if (N <= M / e) ans = e;
+    cout << ans << endl;
+
+    return 0;
+}
