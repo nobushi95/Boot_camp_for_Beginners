b231ed4410c1c6481bad7b4032415e0593b72d2e
nobushi95 <nobushi1121@gmail.com>
2022-07-07 22:08:38 +0900

    add hard100/055

---
+++ hard100/055.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N, A, B;
+    cin >> N >> A >> B;
+    vector<ll> X(N);
+    FORE(x, X) cin >> x;
+
+    ll ans = 0;
+    REP(i, 0, N - 1) {
+        ans += min((X[i + 1] - X[i]) * A, B);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
