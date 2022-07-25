461356c655ca2107fd74a847b26f948d69247388
nobushi95 <nobushi1121@gmail.com>
2022-07-25 21:39:21 +0900

    add hard100/066

---
+++ hard100/066.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    string s;
+    cin >> s;
+
+    ll i = 0, ans = 0, cnt = 0;
+    while (i < s.length()) {
+        if (s[i] == 'A') cnt++;
+        else if (i < s.length() - 1 && s[i] == 'B' && s[i + 1] == 'C') {
+            ans += cnt;
+            i += 1;
+        } else cnt = 0;
+        i++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
