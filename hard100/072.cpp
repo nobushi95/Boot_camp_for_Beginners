104667c9600290a80d93a9b6f9a2dc5ec46b3797
nobushi95 <nobushi1121@gmail.com>
2022-08-02 22:30:40 +0900

    add hard100/072

---
+++ hard100/072.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N;
+    cin >> N;
+
+    REPEQ(h, 1, 3500) REPEQ(n, 1, 3500) {
+        ll denominator = 4 * h * n - N * n - N * h;
+        ll molecule = N * h * n;
+        if (denominator == 0) continue;
+        if (molecule % denominator == 0) {
+            ll w = molecule / denominator;
+            if (w > 0) {
+                cout << h << " " << n << " " << w << endl;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
