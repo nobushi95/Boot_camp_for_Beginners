d5018f011a40d2e390e6fb4efa803128fb206aea
nobushi95 <nobushi1121@gmail.com>
2022-08-12 12:35:08 +0900

    add hard/075

---
+++ hard100/075.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll N, K;
+    string S;
+    cin >> N >> K >> S;
+
+    ll size = 1;
+    char pre = S[0];
+    REP(i, 1, N) {
+        if (pre != S[i]) {
+            pre = S[i];
+            size++;
+        }
+    }
+
+    ll hukou = size;
+    REP(k, 0, K) {
+        if (hukou >= 3) hukou -= 2;
+        else if (hukou == 2) hukou--;
+    }
+
+    cout << N - hukou << endl;
+
+    return 0;
+}
