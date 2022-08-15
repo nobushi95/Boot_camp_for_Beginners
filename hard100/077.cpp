217dcb677aa1bd9e9b28bfaff557404231b91831
nobushi95 <nobushi1121@gmail.com>
2022-08-15 19:42:18 +0900

    add hard/077

---
+++ hard100/077.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> T gcd(const T &a, const T &b) { if (b == 0) return a; else return gcd(b, a % b); }
+template<class T> T lcm(const T &a, const T &b) { return abs(a) / gcd(a, b) * abs(b); }
+
+int main() {
+    ll H, W, A, B;
+    cin >> H >> W >> A >> B;
+
+    REP(i, 0, H) {
+        REP(j, 0, W) {
+            if (i < B && j < A) cout << 0;
+            else if (i < B && j >= A) cout << 1;
+            else if (i >= B && j < A) cout << 1;
+            else if (i >= B && j >= A) cout << 0;
+        }
+        cout << endl;
+    }
+    return 0;
+}
