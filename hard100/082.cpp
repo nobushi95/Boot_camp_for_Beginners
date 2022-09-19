0512203c31d72d02f5c839266f693318fa005d33
nobushi95 <nobushi1121@gmail.com>
2022-09-19 12:17:42 +0900

    add hard/082

---
+++ hard100/082.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+const ll infl = 1LL << 60;
+const int inf = INT_MAX / 2;
+#define REP(i, left, right) for (ll i = left; i < right; i++)
+#define REPEQ(i, left, right) for (ll i = left; i <= right; i++)
+#define RREP(i, right, left) for (ll i = right; i >= left; i--)
+#define FORE(elem, container) for (auto & elem : container)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N), B(N);
+    REP(i, 0, N) cin >> A[i] >> B[i];
+
+    vector<ll> tbl(N);
+    REP(i, 0, N) tbl[i] = A[i] + B[i];
+    sort(ALL(tbl), greater());
+
+    ll ans = -accumulate(ALL(B), 0LL);
+    for (ll i = 0; i < N; i += 2) ans += tbl[i];
+
+    cout << ans << endl;
+
+    return 0;
+}
